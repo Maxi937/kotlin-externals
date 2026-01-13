d06fe810b47eb6eb82c60baf508f4d0d1ca2f17d
Maxi937 <mhornby123@gmail.com>
2026-01-13 19:21:01 +0000

    refactor: remove jnpm snapshot & kotlin pre-release

--- buildSrc/src/main/kotlin/com/github/lppedd/kotlin/gradle/PublishableModulePlugin.kt
+++ buildSrc/src/main/kotlin/com/github/lppedd/kotlin/gradle/PublishableModulePlugin.kt
@@ -1,44 +1,48 @@
 package com.github.lppedd.kotlin.gradle
 
-import com.github.lppedd.kotlin.gradle.ext.booleanProperty
-import com.github.lppedd.kotlin.gradle.ext.mavenRepositoryName
-import com.github.lppedd.kotlin.gradle.ext.mavenRepositoryUrl
 import com.vanniktech.maven.publish.MavenPublishPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
 
-/**
- * @author Edoardo Luppi
- */
 class PublishableModulePlugin : Plugin<Project> {
-  private companion object {
-    private const val SNAPSHOT_SUFFIX = "-SNAPSHOT"
-  }
 
   override fun apply(project: Project) {
     project.apply<MavenPublishPlugin>()
+
     project.afterEvaluate {
-      if (project.booleanProperty("version.snapshot")) {
-        val version = project.version.toString()
-
-        if (!version.endsWith(SNAPSHOT_SUFFIX)) {
-          project.version = version + SNAPSHOT_SUFFIX
-        }
-      }
+      configureGithubPublishing(project)
     }
+  }
 
-    // Allow publishing to a private Maven repository, other than to Maven Central
-    val privateRepoUrl = project.mavenRepositoryUrl
+  private fun configureGithubPublishing(project: Project) {
+    val owner = project.findProperty("github.owner")?.toString()
+      ?: System.getenv("GITHUB_OWNER")
+      ?: error("Missing github.owner")
 
-    if (privateRepoUrl != null) {
-      project.extensions.configure<PublishingExtension>("publishing") {
-        repositories {
-          maven {
-            name = project.mavenRepositoryName ?: "PrivateNexus"
-            url = project.uri(privateRepoUrl)
-            isAllowInsecureProtocol = true
+    val repo = project.findProperty("github.repo")?.toString()
+      ?: System.getenv("GITHUB_REPO")
+      ?: error("Missing github.repo")
+
+    val username = project.findProperty("github.username")?.toString()
+      ?: System.getenv("GITHUB_ACTOR")
+      ?: error("Missing github.username")
+
+    val token = project.findProperty("github.token")?.toString()
+      ?: System.getenv("GITHUB_TOKEN")
+      ?: error("Missing github.token")
+
+    project.extensions.configure<PublishingExtension> {
+      repositories {
+        maven {
+          name = "GitHubPackages"
+          url = project.uri("https://maven.pkg.github.com/$owner/$repo")
+
+          credentials {
+            this.username = username
+            this.password = token
           }
         }
       }
